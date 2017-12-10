cd18dba4c627c1d0d98ac24ebfcaee955b23a09d
Chmoqq <32982711+Chmoqq@users.noreply.github.com>
2017-12-10 18:41:51 +0200

    Added firstPage fragment

---
+++ app/src/main/java/com/example/ivan/lesson1/FragmentMenu.java
@@ -0,0 +1,62 @@
+package com.example.ivan.lesson1;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.example.ivan.lesson1.ActivityButton.StrengthListActivity;
+import com.example.ivan.lesson1.CardioButton.CardioActivity;
+
+public class FragmentMenu extends Fragment {
+    Button activityButton;
+    Button cardioButton;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.from(container.getContext()).inflate(R.layout.fragment_menu, container, false);
+        initUI(root);
+        return root;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    private void initUI(final View root) {
+        activityButton = root.findViewById(R.id.activity_button);
+        cardioButton = root.findViewById(R.id.cardio_button);
+
+
+        activityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Нажата кнопка", Toast.LENGTH_SHORT).show();
+                YoYo.with(Techniques.Shake)
+                        .duration(500)
+                        .repeat(0)
+                        .playOn(root.findViewById(R.id.activity_button));
+                Intent intent = new Intent(getContext(), StrengthListActivity.class);
+                startActivity(intent);
+            }
+        });
+        cardioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "нажата кнопка Кирэк", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getContext(), CardioActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/ivan/lesson1/MainActivity.java
+++ app/src/main/java/com/example/ivan/lesson1/MainActivity.java
@@ -1,48 +1,21 @@
 package com.example.ivan.lesson1;
 
-import android.content.Intent;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-import com.example.ivan.lesson1.ActivityButton.StrengthListActivity;
-import com.example.ivan.lesson1.CardioButton.CardioActivity;
 
 public class MainActivity extends AppCompatActivity {
-    Button activityButton;
-    Button cardioButton;
-
+    FragmentMenu fragmentMenu;
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initUI();
+        fragmentMenu = new FragmentMenu();
+        fragmentManager = getSupportFragmentManager();
 
-    }
+        fragmentManager.beginTransaction().add(R.id.container, fragmentMenu).commit();
 
-    private void initUI() {
-        activityButton = findViewById(R.id.activity_button);
-        cardioButton = findViewById(R.id.cardio_button);
-
-
-        activityButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Нажата кнопка", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(MainActivity.this, StrengthListActivity.class);
-                startActivity(intent);
-            }
-        });
-        cardioButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "нажата кнопка Кирэк", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(MainActivity.this, CardioActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
